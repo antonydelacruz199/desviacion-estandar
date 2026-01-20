b6bb26e015403783ac5d03d3fc87962cc740d62c
adelacruz <76147683@continental.edu.pe>
2026-01-19 19:57:09 -0500

    VERDE: La desviacion estandar generaliza para n elementos

--- src/logica/Desviacion.py
+++ src/logica/Desviacion.py
@@ -9,5 +9,10 @@ class Desviacion:
         if len(self.__datos) == 0:
             return None
 
-        if len(self.__datos) == 1:
-            return 0
+        promedio = sum(self.__datos) / len(self.__datos)
+        suma = 0
+
+        for x in self.__datos:
+            suma += (x - promedio) ** 2
+
+        return math.sqrt(suma / len(self.__datos))
