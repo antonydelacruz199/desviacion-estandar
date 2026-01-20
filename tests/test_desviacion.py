24fa7a936678bc9588741ad144186629ffbc3676
70878805 <70878805@continental.edu.pe>
2026-01-19 19:54:05 -0500

    ROJO: desviacion estandar no funcionar con n elementos

--- tests/test_desviacion.py
+++ tests/test_desviacion.py
@@ -10,3 +10,7 @@ class TestDesviacion(unittest.TestCase):
     def test_un_elemento_retorna_cero(self):
         datos = Desviacion([5])
         self.assertEqual(0, datos.desviacion_estandar())
+
+    def test_n_elementos(self):
+        datos = Desviacion([2, 4, 4, 4, 5, 5, 7, 9])
+        self.assertEqual(2, datos.desviacion_estandar())
