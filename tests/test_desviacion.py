6216d10108972ecddb005a41f5dd12f338770ad9
adelacruz <76147683@continental.edu.pe>
2026-01-19 19:49:25 -0500

    ROJO: La desviacion estandar con un elemento debe ser cero

--- tests/test_desviacion.py
+++ tests/test_desviacion.py
@@ -6,3 +6,7 @@ class TestDesviacion(unittest.TestCase):
     def test_lista_vacia_retorna_none(self):
         datos = Desviacion([])
         self.assertIsNone(datos.desviacion_estandar())
+
+    def test_un_elemento_retorna_cero(self):
+        datos = Desviacion([5])
+        self.assertEqual(0, datos.desviacion_estandar())
